a89e6628b45e20d43a998f63e5068cc1a8f0e7cb
Austin <gilmoursa@gmail.com>
2023-03-16 00:57:42 -0400

    added a temperature conversion feature

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 function convertWeight() {
     const weight = document.getElementById("weight").value;
-    const from = document.getElementById("from").value;
-    const to = document.getElementById("to").value;
-  
-    let result;
-  
-    if (from === "kg" && to === "lbs") {
-      result = weight * 2.20462;
-    } else if (from === "kg" && to === "oz") {
-      result = weight * 35.274;
-    } else if (from === "lbs" && to === "kg") {
-      result = weight / 2.20462;
-    } else if (from === "lbs" && to === "oz") {
-      result = weight * 16;
-    } else if (from === "oz" && to === "kg") {
-      result = weight / 35.274;
-    } else if (from === "oz" && to === "lbs") {
-      result = weight / 16;
+    const weightFrom = document.getElementById("weightFrom").value;
+    const weightTo = document.getElementById("weightTo").value;
+
+    let weightResult;
+
+    if (weightFrom === "kg" && weightTo === "lbs") {
+        weightResult = weight * 2.20462;
+    } else if (weightFrom === "kg" && weightTo === "oz") {
+        weightResult = weight * 35.274;
+    } else if (weightFrom === "lbs" && weightTo === "kg") {
+        weightResult = weight / 2.20462;
+    } else if (weightFrom === "lbs" && weightTo === "oz") {
+        weightResult = weight * 16;
+    } else if (weightFrom === "oz" && weightTo === "kg") {
+        weightResult = weight / 35.274;
+    } else if (weightFrom === "oz" && weightTo === "lbs") {
+        weightResult = weight / 16;
     } else {
-      result = weight;
+        weightResult = weight;
     }
-  
-    document.getElementById("result").value = result.toFixed(2);
-  }
-  
+
+    document.getElementById("weightResult").value = weightResult.toFixed(2);
+}
+
+function convertTemperature() {
+    const temperature = document.getElementById("temperature").value;
+    const tempFrom = document.getElementById("tempFrom").value;
+    const tempTo = document.getElementById("tempTo").value;
+
+    let result;
+
+    if (tempFrom === "c" && tempTo === "f") {
+        tempResult = (temperature * 9 / 5) + 32;
+    } else if (tempFrom === "c" && tempTo === "k") {
+        tempResult = parseFloat(temperature) + 273.15;
+    } else if (tempFrom === "f" && tempTo === "c") {
+        tempResult = (temperature - 32) * 5 / 9;
+    } else if (tempFrom === "f" && tempTo === "k") {
+        tempResult = (parseFloat(temperature) + 459.67) * 5 / 9;
+    } else if (tempFrom === "k" && tempTo === "c") {
+        tempResult = parseFloat(temperature) - 273.15;
+    } else if (tempFrom === "k" && tempTo === "f") {
+        tempResult = (parseFloat(temperature) * 9 / 5) - 459.67;
+    } else {
+        tempResult = temperature;
+    }
+
+    document.getElementById("tempResult").value = tempResult.toFixed(2);
+}
